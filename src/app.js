6cbc1b9a1853d104e0d91c56e7406a9a8b6ccd9a
Zylau <zylau@live.fr>
2023-01-12 05:47:07 +0100

    ajout de patterns dans le localStorage

--- src/app.js
+++ src/app.js
@@ -52,13 +52,15 @@ printPatterns()
 
 // Event handler
 
-// let btn = document.querySelector('#btn-add')
-// btn.addEventListener('click', event => {
-// 	let name = document.getElementById('name')
-// 	let code = document.getElementById('code')
-// 	let coment = document.getElementById('coment')
-// 	addPattern(name.value, code.value, coment.value)
-// })
+let btn = document.querySelector('#btn-add')
+if (btn) {
+	btn.addEventListener('click', event => {
+		let name = document.getElementById('name')
+		let code = document.getElementById('code')
+		let coment = document.getElementById('coment')
+		addPattern(name.value, code.value, coment.value)
+	})
+}
 
 let btnDel = document.querySelectorAll('button[name="btn-del"]')
 btnDel.forEach((btn) => {
