0e444daded4d3cc8bc453d0b5023993de487541f
Zylau <zylau@live.fr>
2023-01-12 05:15:45 +0100

    deployment test

--- src/app.js
+++ src/app.js
@@ -1,8 +1,85 @@
+// Fuctions definition
 
-function sum (a, b) {
-	return a+b
+function printPatterns () {
+	if (localStorage.length != 0) {
+		document.getElementById('list').innerHTML = ''
+	
+		Object.keys(localStorage).forEach((key) => {
+			let val = JSON.parse(localStorage[key])
+			let li = document.createElement('li')
+	
+			let name = document.createElement('h3')
+			name.textContent = key
+			li.appendChild(name)
+			
+			let code = document.createElement('p')
+			code.textContent = val.code
+			li.appendChild(code)
+	
+			let coment = document.createElement('p')
+			coment.textContent = val.coment
+			li.appendChild(coment)
+
+			let suppr = document.createElement("button")
+			suppr.id = key
+			suppr.name = 'btn-del'
+			suppr.textContent = 'Delete'
+			li.appendChild(suppr)
+	
+			document.getElementById('list').appendChild(li)
+		})	
+	}
 }
 
+function addPattern (name, code, coment) {
+	let val = {code: code, coment: coment}
+	localStorage.setItem(name, JSON.stringify(val))
+	printPatterns()
+}
+
+function delPattern (name) {
+	localStorage.removeItem(name)
+	printPatterns()
+}
+
+
+// Main
+
+printPatterns()
+
+// Main
+
+
+// Event handler
+
+let btn = document.querySelector('#btn-add')
+btn.addEventListener('click', event => {
+	let name = document.getElementById('name')
+	let code = document.getElementById('code')
+	let coment = document.getElementById('coment')
+	addPattern(name.value, code.value, coment.value)
+})
+
+let btnDel = document.querySelectorAll('button[name="btn-del"]')
+btnDel.forEach((btn) => {
+	console.log(btn)
+	btn.addEventListener('click', event => {
+		let name = btn.id
+		console.log(name)
+		delPattern(name)
+	})
+})
+
+
+function sum (a, b) {
+	return a + b
+}
+
+
 if (typeof module === 'object') {
-	module.exports = sum
+	module.exports = {
+		sum,
+		addPattern,
+		delPattern
+	}
 }
