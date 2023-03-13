5793e458d422a6662d1c50a2a9faa099d253e650
Zylau <zylau@live.fr>
2023-03-14 00:13:31 +0100

    TU update

--- src/app.test.js
+++ src/app.test.js
@@ -3,30 +3,53 @@
  */
 
 const app = require("./app.js")
-window.alert = jest.fn()
+const localStorage = {
+	setItem: jest.fn(),
+	removeItem: jest.fn()
+}
 
-describe('test addPattern', () => {
-	
+
+describe('Test addPattern', () => {
+	alert = jest.fn()
+
 	test('return -1 with name="" code="" coment=""', () => {
 		expect(app.addPattern("", "", "")).toBe(-1)
+		expect(alert).toHaveBeenCalled()
 	})
 	test('return -1 with name="d" code="" coment=""', () => {
 		expect(app.addPattern("d", "", "")).toBe(-1)
+		expect(alert).toHaveBeenCalled()
 	})
 	test('return -1 with name="" code="d" coment=""', () => {
 		expect(app.addPattern("", "d", "")).toBe(-1)
+		expect(alert).toHaveBeenCalled()
 	})
 	test('return -1 with name="" code="" coment="d"', () => {
 		expect(app.addPattern("", "", "d")).toBe(-1)
+		expect(alert).toHaveBeenCalled()
 	})
 	test('return -1 with name="d" code="d" coment=""', () => {
 		expect(app.addPattern("d", "d", "")).toBe(-1)
+		expect(alert).toHaveBeenCalled()
 	})
 	test('return -1 with name="d" code="" coment="d"', () => {
 		expect(app.addPattern("d", "", "d")).toBe(-1)
+		expect(alert).toHaveBeenCalled()
 	})
 	test('return -1 with name="" code="d" coment="d"', () => {
 		expect(app.addPattern("", "d", "d")).toBe(-1)
+		expect(alert).toHaveBeenCalled()
 	})
 
+	test('setItem in localStorage', () => {
+		//app.addPattern("d", "d", "d")
+		//expect(localStorage.setItem).toHaveBeenCalled()
+	})
+
+})
+
+describe ('Test delPattern', () => {
+	test('removeItem in localStorage', () => {
+		//expect(localStorage.removeItem).toHaveBeenCalled()
+	})
 })
